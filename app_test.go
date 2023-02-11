2258e66adda4125245f68dfa879cedcac02ee499
harsh <harsh@nurture.farm>
2023-02-11 21:54:56 +0530

    Added test for sending data and subscribing.

--- app_test.go
+++ app_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"net"
+	"runtime"
 	"testing"
 	"time"
 
@@ -20,7 +22,93 @@ func TestConnection(t *testing.T) {
 		stomp.ConnOpt.HeartBeat(5*time.Millisecond, 5*time.Millisecond),
 	)
 	if client == nil || err != nil {
-		t.Fatalf(`We have an error. Connection is not eastablished`)
+		t.Fatalf(`We have an error. Client Connection is not eastablished`)
 	}
+
+	err = client.Send("/topic/whatever", "text/plain", []byte("hello Mr. X! you were awaited."))
+
+	if client == nil || err != nil {
+		t.Fatalf(`We have an error. Client Connection is not sending data to a topic`)
+	}
+
 	defer client.Disconnect()
+}
+
+func TestSendToQueuesAndTopics(t *testing.T) {
+	fmt.Println("-------- Started Testing -----")
+	ch := make(chan bool, 2)
+	fmt.Println("number cpus:", runtime.NumCPU())
+
+	addr := ":59091"
+
+	// channel to communicate that the go routine has started
+	started := make(chan bool)
+
+	count := 100
+	go runReceiver(ch, count, "/topic/test-1", addr, started)
+	<-started
+	go runReceiver(ch, count, "/topic/test-1", addr, started)
+	<-started
+	go runReceiver(ch, count, "/topic/test-2", addr, started)
+	<-started
+	go runReceiver(ch, count, "/topic/test-2", addr, started)
+	<-started
+	go runReceiver(ch, count, "/topic/test-1", addr, started)
+	<-started
+	go runReceiver(ch, count, "/queue/test-1", addr, started)
+	<-started
+	go runSender(ch, count, "/queue/test-1", addr, started)
+	<-started
+	go runSender(ch, count, "/queue/test-2", addr, started)
+	<-started
+	go runReceiver(ch, count, "/queue/test-2", addr, started)
+	<-started
+	go runSender(ch, count, "/topic/test-1", addr, started)
+	<-started
+	go runReceiver(ch, count, "/queue/test-3", addr, started)
+	<-started
+	go runSender(ch, count, "/queue/test-3", addr, started)
+	<-started
+	go runSender(ch, count, "/queue/test-4", addr, started)
+	<-started
+	go runSender(ch, count, "/topic/test-2", addr, started)
+	<-started
+	go runReceiver(ch, count, "/queue/test-4", addr, started)
+	<-started
+
+	for i := 0; i < 15; i++ {
+		<-ch
+	}
+}
+
+func runSender(ch chan bool, count int, destination, addr string, started chan bool) {
+	conn, _ := net.Dial("tcp", "127.0.0.1"+addr)
+
+	client, _ := stomp.Connect(conn)
+
+	started <- true
+
+	for i := 0; i < count; i++ {
+		client.Send(destination, "text/plain",
+			[]byte(fmt.Sprintf("%s Hi! Mr X. You were expected. your id is:  %d", destination, i)))
+		//println("sent", i)
+	}
+
+	ch <- true
+}
+
+func runReceiver(ch chan bool, count int, destination, addr string, started chan bool) {
+	conn, _ := net.Dial("tcp", "127.0.0.1"+addr)
+
+	client, _ := stomp.Connect(conn)
+
+	sub, _ := client.Subscribe(destination, stomp.AckAuto)
+
+	started <- true
+
+	for i := 0; i < count; i++ {
+		msg := <-sub.C
+		fmt.Println("----------------   received Message  ------------  ", string(msg.Body))
+	}
+	ch <- true
 }
